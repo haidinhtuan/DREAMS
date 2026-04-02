ef2807748ac2ae92421d64935df3bf4e572a5570
Tuan Hai Dinh <tuan-hai.dinh@ionos.com>
2026-04-02 13:39:26 +0200

    feat: add coupling, functional, operational, privacy affinity methods to interface

--- src/main/java/com/dreams/domain/service/AffinityCalculationService.java
+++ src/main/java/com/dreams/domain/service/AffinityCalculationService.java
@@ -8,6 +8,14 @@ import java.util.Map;
 public interface AffinityCalculationService {
     double calculateDataAffinity(Microservice u, Microservice v);
 
+    double calculateCouplingAffinity(Microservice u, Microservice v);
+
+    double calculateFunctionalAffinity(Microservice u, Microservice v);
+
+    double calculateOperationalAffinity(Microservice u, Microservice v);
+
+    double calculatePrivacyAffinity(Microservice u, Microservice v);
+
     double calculateTotalAffinity(Microservice u, Microservice v, Map<String, Double> weights);
 
     double calculateTotalAffinityImpact(Microservice migratingMicroservice, List<Microservice> microservices);
