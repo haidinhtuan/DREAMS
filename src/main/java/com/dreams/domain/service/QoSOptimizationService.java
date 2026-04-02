96b40be04237d0b337eaaab4063096cf9118bb73
Tuan Hai Dinh <tuan-hai.dinh@ionos.com>
2026-04-02 16:13:15 +0200

    feat: add findAllMigrationCandidates to QoSOptimizationService interface

--- src/main/java/com/dreams/domain/service/QoSOptimizationService.java
+++ src/main/java/com/dreams/domain/service/QoSOptimizationService.java
@@ -3,9 +3,13 @@ package com.dreams.domain.service;
 import com.dreams.domain.model.Microservice;
 import com.dreams.domain.model.MigrationCandidate;
 
+import java.util.List;
+
 public interface QoSOptimizationService {
 
     MigrationCandidate findBestMigrationCandidate();
+
+    List<MigrationCandidate> findAllMigrationCandidates();
 
     boolean shouldApproveMigrationProposal(Microservice migratingMicroservice, String targetLdmId);
 }
